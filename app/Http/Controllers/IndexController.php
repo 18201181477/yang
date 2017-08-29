143bcd472b60a4ab07220059cc23463a5248775c
xiaoqing <1379804332@qq.com>
2017-08-29 14:26:30 +0800

    行

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -1,16 +1,11 @@
 <?php
-
 namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-
-use App\Http\Requests;
-use App\Http\Controllers\Controller;
 
 class IndexController extends Controller
 {
-    public function index()
-    {
-    	return view('index.index');
-    }
+	
+	public function index()
+	{
+		return view('Index.index');
+	}
 }
